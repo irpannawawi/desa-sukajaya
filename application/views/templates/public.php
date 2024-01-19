38d9ac947b52b996055bd8f3b756bedd2168fd9d
irpannawawi <irpannawawi.ixd@gmail.com>
2024-01-19 23:19:49 +0700

    fix typo

--- application/views/templates/public.php
+++ application/views/templates/public.php
@@ -45,7 +45,7 @@
     <header id="header" class="fixed-top">
         <div class="container d-flex align-items-center">
 
-            <h1 class="logo me-auto"><a href="<?= site_url('/') ?>">Deas Sukajaya</a></h1>
+            <h1 class="logo me-auto"><a href="<?= site_url('/') ?>">Desa Sukajaya</a></h1>
             <!-- Uncomment below if you prefer to use an image logo -->
             <!-- <a href="index.html" class="logo me-auto"><img src="<?= base_url('assets/theme/Arsha/') ?>assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
